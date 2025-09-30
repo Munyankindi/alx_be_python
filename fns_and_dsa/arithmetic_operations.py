eac469c422442cb91103a133de2dac1a46e594b4
Munyankindi <guybertrandm@gmail.com>
2025-09-30 19:05:52 +0200

    added fns_and_dsa

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,14 @@
+def perform_operation(num1,num2,operation):
+    num1 =float(num1)
+    num2 = float(num2)
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == "divide":
+        if num2 != 0:
+            return num1 / num2
+        else:
+            return "invalid operation"
